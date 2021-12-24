fb125b0a3e5a5883ede025794428f25c495e3615
Raphael Kusumo Hendrianto <71378076+RaphaelKusumo@users.noreply.github.com>
2021-12-24 21:29:48 +0700

    Add files via upload

---
+++ Data-Mining-Student-Performance-master/Data-Mining-Student-Performance-master/knn.py
@@ -0,0 +1,40 @@
+import numpy as np
+import pandas as pd
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+import sklearn.metrics
+
+df = pd.read_csv(r'''D:\RAPHAEL\semester 7\machine learning\Data-Mining-Student-Performance-master\Data-Mining-Student-Performance-master\datat.csv''')
+df['sex'] = df['sex'].map({'M': 0, 'F': 1})
+df['address'] = df['address'].map({'U': 0, 'R': 1})
+df['guardian'] = df['guardian'].map({'mother': 0, 'father': 1})
+#df['internet'] = df['internet'].map({'no': 0, 'yes': 1})
+
+
+predictors = df.values[:, 0:11]
+targets = df.values[:,12]
+
+pred_train, pred_test, tar_train, tar_test = train_test_split(predictors, targets, test_size= 0.25)
+
+
+print(pred_train.shape)
+print(pred_test.shape)
+print(tar_train.shape)
+print(tar_test.shape)
+
+neigh = KNeighborsClassifier(n_neighbors = 1, weights='uniform', algorithm='auto')
+neigh.fit(pred_train, tar_train)
+y_pred = neigh.predict(pred_test)
+
+
+#accuracy
+print("Accuracy is ", accuracy_score(tar_test, y_pred, normalize = True))
+#classification error
+print("Classification error is",1- accuracy_score(tar_test, y_pred, normalize = True))
+#sensitivity
+print("sensitivity is", sklearn.metrics.recall_score(tar_test, y_pred, labels=None, average =  'micro', sample_weight=None))
+#specificity
+print("specificity is", 1 - sklearn.metrics.recall_score(tar_test, y_pred,labels=None, average =  'micro', sample_weight=None))
